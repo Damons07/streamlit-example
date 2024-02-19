a52c7cb2b34e3f4bed9c222ac8fab350cf82f0d3
Damons07 <damianedg666@gmail.com>
2024-02-19 13:00:02 -0600

    naufragios prueba

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,32 +10,6 @@ collection = db.shipwrecks
 # Obtener datos de la colección
 data = pd.DataFrame(list(collection.find()))
 
-# Cambiar los nombres de las columnas al español
-nombres_columnas_espanol = {
-    'Feature Type': 'Tipo de Característica',
-    'Chart': 'Carta',
-    'Latitude': 'Latitud',
-    'Longitude': 'Longitud',
-    'Gazetteer': 'Gazetteer',
-    'Depth': 'Profundidad',
-    'History': 'Historia',
-    'Description': 'Descripción',
-    'Accession No.': 'Número de Acceso',
-    'Catalog No.': 'Número de Catálogo',
-    'Object Name': 'Nombre del Objeto',
-    'Vessel Name': 'Nombre del Buque',
-    'Event': 'Evento',
-    'Year Built': 'Año de Construcción',
-    'Year Sunk': 'Año Hundido',
-    'Reason Sunk': 'Razón del Hundimiento',
-    'Coordinates': 'Coordenadas',
-    'Legal Status': 'Estado Legal',
-    'Date': 'Fecha',
-    'Remarks': 'Observaciones'
-}
-
-data.rename(columns=nombres_columnas_espanol, inplace=True)
-
 # Mostrar dataframe antes de la limpieza
 st.write("Dataframe antes de la limpieza:")
 st.write(data)
